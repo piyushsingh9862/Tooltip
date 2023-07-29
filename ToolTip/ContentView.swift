b5a0a897260c8961982efda81aa307ad0e82655e
PIYUSH SINGH <piyushsingh9862@gmail.com>
2023-07-29 14:37:27 +0530

    Initial Commit

---
+++ ToolTip/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToolTip
+//
+//  Created by Piyush Singh on 29/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToolTip/ToolTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolTipApp.swift
+//  ToolTip
+//
+//  Created by Piyush Singh on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ToolTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
